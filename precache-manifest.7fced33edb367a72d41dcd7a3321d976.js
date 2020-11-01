3a70d5c8a8a3570a3cc86d3e86ad438b41c23c4a
Mahmoud <hamdan.mahmoudtudor@gmail.com>
2020-11-01 21:47:32 +0200

    Updates

---
+++ precache-manifest.7fced33edb367a72d41dcd7a3321d976.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f7836faa85f1149716d14364fb3f7f",
+    "url": "/unifly/index.html"
+  },
+  {
+    "revision": "9e55efc9f9a96810b5a8",
+    "url": "/unifly/static/css/main.f94936ad.chunk.css"
+  },
+  {
+    "revision": "f75e09b1fe9580dd5ad9",
+    "url": "/unifly/static/js/2.73e9306d.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/unifly/static/js/2.73e9306d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e55efc9f9a96810b5a8",
+    "url": "/unifly/static/js/main.d02eb536.chunk.js"
+  },
+  {
+    "revision": "eea076b9e4a6a30c0994",
+    "url": "/unifly/static/js/runtime-main.137d16c3.js"
+  }
+]);
